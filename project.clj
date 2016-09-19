1304bb0bafdf43438807685b602937d57335e23b
Amar Mehta <xfthhxk@gmail.com>
2016-09-19 14:20:09 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject e85th/backend "0.1.0-SNAPSHOT"
+(defproject e85th/backend "0.1.0"
   :description "Backend server code."
   :url "https://github.com/e85th/backend"
   :license {:name "Apache License 2.0"
