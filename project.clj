fe69b7cdc034d972e5533a801ae35b5b31a77f72
Amar Mehta <xfthhxk@gmail.com>
2016-09-30 10:14:16 -0400

    bump minor version, add new method in user ns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject e85th/backend "0.1.3"
+(defproject e85th/backend "0.1.4"
   :description "Backend server code."
   :url "https://github.com/e85th/backend"
   :license {:name "Apache License 2.0"
