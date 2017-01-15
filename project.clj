eb06832523298522f01e637fef26faa22b49a98e
Amar Mehta <xfthhxk@gmail.com>
2017-01-15 15:51:02 -0500

    bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject e85th/backend "0.1.13"
+(defproject e85th/backend "0.1.14"
   :description "Backend server code."
   :url "https://github.com/e85th/backend"
   :license {:name "Apache License 2.0"
