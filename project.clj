9a8efa968da819d364e733d2cf8d685581644ea4
Amar Mehta <xfthhxk@gmail.com>
2017-09-24 21:52:30 -0400

    bump deps, version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject e85th/backend "0.1.36-alpha2"
+(defproject e85th/backend "0.1.36"
   :description "Backend server code."
   :url "https://github.com/e85th/backend"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1" :scope "provided"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [e85th/commons "0.1.25-alpha1"]
+                 [e85th/commons "0.1.25"]
                  [com.taoensso/timbre "4.10.0"] ; explicitly include
                  [org.clojure/core.async "0.3.443"] ;; override sente version for spec ns
                  [com.taoensso/sente "1.11.0"] ; websockets
