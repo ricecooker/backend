e45f03acdc4f552a440d97363e89db1ea84ad04a
Amar Mehta <xfthhxk@gmail.com>
2017-09-24 21:04:52 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject e85th/backend "0.1.35"
+(defproject e85th/backend "0.1.36-alpha2"
   :description "Backend server code."
   :url "https://github.com/e85th/backend"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1" :scope "provided"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [e85th/commons "0.1.24"]
+                 [e85th/commons "0.1.25-alpha1"]
                  [com.taoensso/timbre "4.10.0"] ; explicitly include
                  [org.clojure/core.async "0.3.443"] ;; override sente version for spec ns
                  [com.taoensso/sente "1.11.0"] ; websockets
                  [com.layerware/hugsql "0.4.7"]
-                 [metosin/compojure-api "2.0.0-alpha4"]
+                 [metosin/compojure-api "2.0.0-alpha7"]
                  [metosin/ring-http-response "0.9.0"]
                  [ring-cors "0.1.11" :exclusions [ring/ring-core]]
                  [org.immutant/web "2.1.9" :exclusions [ring/ring-core]]
                  [http-kit "2.2.0"]
-                 [buddy "1.3.0"]
+                 [buddy "2.0.0"]
                  [com.google.api-client/google-api-client "1.22.0" :scope "provided"]
                  [com.google.firebase/firebase-server-sdk "3.0.3" :scope "provided"]
                  [org.clojure/tools.nrepl "0.2.13" :scope "provided"]]
